1f9ed3c8b996cd437b952af6cdf9c2655670eb98
Ruth Cao <ruoshen.c@gmail.com>
2021-06-11 15:41:14 -0400

    remove the excludes parameter and change the type to number for numeric parameters

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -9,6 +9,6 @@ def lambda_handler(event, context):
     logger.info('## ENVIRONMENT VARIABLES')
     logger.info(os.environ)
 
-    weatherURL = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&units={units}&appid={apiKey}'.format(lat=os.environ['Latitude'], lon=os.environ['Longtitude'], part=os.environ['Excludes'], units=os.environ['Units'], apiKey=os.environ['WeatherApiKey'])
+    weatherURL = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units={units}&appid={apiKey}'.format(lat=os.environ['Latitude'], lon=os.environ['Longtitude'], units=os.environ['Units'], apiKey=os.environ['WeatherApiKey'])
     notifier = WeatherNotifier(os.environ['NotifyAlexURL'], os.environ['NotifyAlexAccessCode'], os.environ['TopicArn'], weatherURL, os.environ['Atmosphere'], os.environ['Probability'])
     notifier.notify_weather()
