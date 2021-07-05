4b8f7a95357194b6bcee61a0478c99c525910523
Ruth Cao <ruoshen.c@gmail.com>
2021-07-05 11:00:46 -0400

    retrieve from aws secret manager

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,6 +1,9 @@
 import os
 import logging
+import boto3
+import base64
 from weather import WeatherNotifier
+from botocore.exceptions import ClientError
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -8,6 +11,50 @@ logger.setLevel(logging.INFO)
 def lambda_handler(event, context):
     logger.info('## ENVIRONMENT VARIABLES')
     logger.info(os.environ)
+
+    secret_name = "weatherapikey"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+    logger.info(secret)
+    logger.info(decoded_binary_secret)
 
     weatherURL = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units={units}&appid={apiKey}'.format(lat=os.environ['Latitude'], lon=os.environ['Longtitude'], units=os.environ['Units'], apiKey=os.environ['WeatherApiKey'])
     notifier = WeatherNotifier(os.environ['NotifyAlexURL'], os.environ['NotifyAlexAccessCode'], os.environ['TopicArn'], weatherURL, os.environ['Atmosphere'], os.environ['Probability'])
